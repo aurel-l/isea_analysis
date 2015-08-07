8f1c00940d9cd4d8dc4010bd37976be7d37ad539
Aurélien Luciani <aurelien.luciani@gmail.com>
2015-08-07 13:41:07 +1200

    benchmark scripts

--- benchmark/benchmark-merge.sh
+++ benchmark/benchmark-merge.sh
@@ -2,8 +2,8 @@
 
 nIslands=21
 
-min=50
-max=7500
+min=1
+max=33600
 
 loop=1
 
@@ -11,16 +11,16 @@ trap 'printf "\033[0m\n"; rm .tmp; rm stats/$(ls -t stats | head -n 1); break' S
 while true
 do
   r=$(shuf -i $min-$max -n 1)
-  let "nRows=$nIslands*$r+1"
+  let "nRows=$r+1"
   
   printf "Preparing benchmark with $r simulations…\n"
   
-  ../Python/db_query.py -c "batch_param.date = '2015-07-08 02:34:37' AND startingDistributionFile = 'starting_distribution_3.csv'" 2> /dev/null | head -n $nRows > .tmp
+  head -n $nRows adm.batch_param_map.txt > .tmp
   
   printf "Performing benchmark…\n\033[1;34m"
   
   statFileName=$(printf "%06d" $loop)-${r}-$(wc -c <".tmp").stats
-  /usr/bin/time -v -o stats/$statFileName ../R/analysis.R -rv ../Data/isea_admixture_data_for_comparison_2.csv ../Data/isea_admixture_data_for_comparison_2.csv .tmp 2> /dev/null
+  /usr/bin/time -v -o stats/$statFileName ../R/merge.R -p adm.txt .tmp > /dev/null
   
   [ $? -ne 0 ] && exitColor="\033[1;31m" || exitColor="\033[1;32m"
   printf "${exitColor}…done\033[0m\n"
--- benchmark/parse.sh
+++ benchmark/parse.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-regexFileName='^0*([1-9][0-9]*)-([0-9]*).stats$'
-regexTime='([1-5]?[0-9]):([0-9]{2}).([0-9]{2})$'
+regexFileName='^0*([1-9][0-9]*)-([0-9]+)-([0-9]+)\.stats$'
+regexTime='([1-5]?[0-9]):([0-9]{2})\.([0-9]{2})$'
 regexMem='([0-9]+)$'
 regexExit='([0-1])$'
 
-echo 'index, nSimulations, time, maxMem, failed'
+echo 'index, nSimulations, inputFileSize, time, maxMem, failed'
 
 for fileName in $(ls stats)
 do
   if [[ -s stats/$fileName ]]
   then
     [[ $fileName =~ $regexFileName ]]
-    printf "${BASH_REMATCH[1]}, ${BASH_REMATCH[2]}, "
+    inputFileSize=$(bc -l <<< "${BASH_REMATCH[3]}/1024/1024")
+    printf "${BASH_REMATCH[1]}, ${BASH_REMATCH[2]}, $inputFileSize, "
     
     time=$(grep "wall clock" stats/$fileName)
     [[ $time =~ $regexTime ]]
